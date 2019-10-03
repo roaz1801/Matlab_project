d3c08032f497cec18594932eb036fc3aa23bc05d
roshanna <roshanna@student.matnat.uio.no>
2019-10-03 13:57:14 +0200

    Everything now works and is ready for testing

--- Oppgave1.m
+++ Oppgave1.m
@@ -2,17 +2,21 @@
 f1 = 10 %Frekvens 1
 f2 = 20 %Frekvens 2
 fs = 100 %Samplingsfrekvens 
-t = 5 %Time duration
-n = [0:1/fs:t]
-x = sin(2*pi*10*n)+sin(2*pi*20*n)
-h = 1
+t = [0:1/fs:5] %Time duration
+t1 = [0:length(x)-1]*1/fs
+t2 = [0:length(x)+length(h)-2]*1/fs
 
-y = konvin3190(h,x,1)
+x = sin(2*pi*f1*t)+sin(2*pi*f2*t)
 
-[X,f] = frekspekin3190(x,3,fs)
+h = [0.2,0.2,0.2,0.2,0.2]
+
+y = konvin3190(h,x,0)
+
+[X,f] = frekspekin3190(x,1000,fs)
 
 %z = conv(h,x)
-plot(n,y)
+%plot(f,X)
+plot(f,abs(X))
 %freqz(x)
 
 
--- frekspekin3190.m
+++ frekspekin3190.m
@@ -1,8 +1,10 @@
 function [X,f] = frekspekin3190(x, N, fs)
-    M = length(x)
-    omega = (2*pi)/3
-    f = [0:fs/2]
-    for k = 1:M
-        X(k) = abs(x(k)*exp((-i)*omega*k))
+    M = length(x);
+    f_hat = [0:N-1]/N;
+    f = f_hat*fs;
+    X = zeros(N,1);
+    for n = 1:N
+        for k=1:M
+        X(n) = X(n)+x(k)*exp((-i)*2*pi*f_hat(n)*k);
     end
 end
