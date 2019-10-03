35c0292fcaf3c1b2c727e8470998c4dc93458fc9
roaz1801 <54395470+roaz1801@users.noreply.github.com>
2019-10-03 20:06:38 +0200

    Updated files with plot

--- Oppgave1.m
+++ Oppgave1.m
@@ -1,23 +1,44 @@
-
 f1 = 10 %Frekvens 1
 f2 = 20 %Frekvens 2
 fs = 100 %Samplingsfrekvens 
 t = [0:1/fs:5] %Time duration
-t1 = [0:length(x)-1]*1/fs
+h = [0.2,0.2,0.2,0.2,0.2] %FIR filter fra oppgave 1
+N = 5000; %Antall punkter for frekvensspekteret.
+x = sin(2*pi*f1*t)+sin(2*pi*f2*t) %Signalet vi behandler
+
+%Vi vil ha forksjellig lengde på t avhengi av om lengden vi velger på y.
+t1 = [0:length(x)-1]*1/fs 
 t2 = [0:length(x)+length(h)-2]*1/fs
 
-x = sin(2*pi*f1*t)+sin(2*pi*f2*t)
-
-h = [0.2,0.2,0.2,0.2,0.2]
-
+%Konvolverer y, kan velge mellom ylen=1 eller 0.
 y = konvin3190(h,x,0)
 
-[X,f] = frekspekin3190(x,1000,fs)
+%Finner frekvensspekteret av de forksjellige signalene/filtrene
+[Xh,fh] = frekspekin3190(h,N,fs)
+[Xx,fx] = frekspekin3190(x,N,fs)
+[Xy,fy] = frekspekin3190(y,N,fs)
 
-%z = conv(h,x)
-%plot(f,X)
-plot(f,abs(X))
-%freqz(x)
+%Plotter figurene
+figure
+plot(fh,abs(Xh))
+title('Frekvensspekter av h')
+xlabel('Frekvens')
+ylabel('Amplitude')
+
+
+figure
+subplot(2,1,1);
+plot(fy,abs(Xy))
+title('Frekvensspekter av x og h konvolvert')
+xlabel('Frekvens')
+ylabel('Amplitude')
+
+
+subplot(2,1,2);
+plot(fx,abs(Xx))
+title('Frekvensspekter av x signal')
+xlabel('Frekvens')
+ylabel('Amplitude')
 
 
 
--- frekspekin3190.m
+++ frekspekin3190.m
@@ -1,10 +1,14 @@
 function [X,f] = frekspekin3190(x, N, fs)
-    M = length(x);
-    f_hat = [0:N-1]/N;
-    f = f_hat*fs;
-    X = zeros(N,1);
+%   [X,f] = frekspekin3190(x,N,fs) regner ut frekvensresponsen til
+%   signalet x med samplingsfrekvens fs for N punkter på enhetssirkelen.
+%   I tillegg til frekvensspekteret X returnerer den også tilhørende 
+%   frekvens f.
+    M = length(x); %Lengde til signal
+    f_hat = [0:N-1]/N;  %Antall punkter
+    f = f_hat*fs;   %Tilhørende frekvens til frekvensspekteret
+    X = zeros(N,1); 
     for n = 1:N
         for k=1:M
-        X(n) = X(n)+x(k)*exp((-i)*2*pi*f_hat(n)*k);
+        X(n) = X(n)+x(k)*exp((-i)*2*pi*f_hat(n)*k); %Fourier transform
     end
 end
